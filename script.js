6104b1c454a9fd66390f9f80a436f2a6b46ff17e
cigsncodes <161231447+cigsncodes@users.noreply.github.com>
2024-07-24 16:50:02 -0300

    kkkkkkk

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ let minhalistadeitens = []
 function addnewtask() {
     minhalistadeitens.push(input.value);
     
+    mostrartarefas()
 }
 
 function mostrartarefas(){
-    
+
+    let novaLi = ''
+
+    minhalistadeitens.forEach(tarefa => {
+
+        novaLi = ´            <li>
+        <img src="" alt="">
+        <p>fazer compras</p>
+        <img src="" alt="">
+    </li> ´
+        
+    });
+
+    <li>
+    <img src="" alt="">
+    <p>fazer compras</p>
+    <img src="" alt="">
+</li>
 }
 button.addEventListener("click", addnewtask)
